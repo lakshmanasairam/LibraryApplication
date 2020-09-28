189a29a3a2ee27a454bd834589bac5ffa888c080
lakshmanasairam <sairamkota234@gmail.com>
2020-09-28 14:26:49 +0530

    Adding NewController Accept it saiteja

---
+++ LibraryApplication/Controllers/LakshmanController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApplication.Controllers
+{
+    public class LakshmanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
